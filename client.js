e1794422aee34a949300f736aae262c95b51d69d
Liuyi Chang <liuyi.chang@outlook.com>
2024-05-09 13:11:35 +0800

    loops activity

--- client.js
+++ client.js
@@ -6,7 +6,11 @@
  * 3. After the loop, log out the total number of
  *    hobbies
  */
-
+let hobbies = ["swimming", "cooking", "eating"];
+for (i=0; i<hobbies.length; i++){
+     console.log (`${i+1}. ${hobbies[i]}`);
+}
+console.log (`I have ${hobbies.length} hobbies.`);
 
 // Example output
 // 1. swimming
@@ -24,6 +28,15 @@
  *    is in the array
  * 4. Output the array and number of times teal was found
  */
+let colors = ["green", "blue", "teal green", "teal blue"];
+let tealCount = 0;
+for (let colorsItems of colors){
+     if (colorsItems.includes('teal')){
+          tealCount += 1;
+     }
+}
+console.log (`Colors include ${colors}`);
+console.log(`There are ${tealCount} colors include "teal".`);
 
 
 // Example output
@@ -40,7 +53,20 @@
  *    array and even numbers in the evenNumbers array.
  * 4. Output the original array, odd number array and even number array
  */
-
+let numbersArray = [1, 3, 4, 6, 2, 8, 9];
+let oddNumbers = [];
+let evenNumbers = [];
+for (let numbersItems of numbersArray){
+     if (numbersItems % 2 === 0){
+          evenNumbers.push(numbersItems);
+     }
+     else {
+          oddNumbers.push(numbersItems);
+     }
+}
+console.log(`Nubers are ${numbersArray}`);
+console.log(`Odd numbers are ${oddNumbers}`);
+console.log(`Even numbers are ${evenNumbers}`);
 
 // Example output
 // 3, 7, 2, 8, 11, 4, 2
@@ -56,7 +82,18 @@
  * 3. Write a loop that adds the opposite value to the toggled array
  * 4. Output both arrays
  */
-
+let booleanArray = [true, false, false, true, false, false];
+let oppositeArray = [];
+for (let booleanItems of booleanArray){
+     if (booleanItems === true){
+          oppositeArray.push (false);
+     }
+     else {
+          oppositeArray.push (true);
+     }
+}
+console.log (`The original array is: ${booleanArray}`);
+console.log (`The false array is: ${oppositeArray}`);
 
 // Example output
 // true, false, true, true
@@ -74,7 +111,17 @@
  *
  * Hint: Try using a while loop for this one.
  */
-
+let zerosIncludedArray = [3, 0, 2, 8, 0, 0, 0];
+console.log (`Before loop, the array is ${zerosIncludedArray}`);
+for (let i=zerosIncludedArray.length-1; i>=0; i--) {
+     if (zerosIncludedArray[i] === 0){
+          zerosIncludedArray.pop();
+     }
+     else {
+          break;
+     }
+}
+console.log(`After loop, the array is ${zerosIncludedArray}`);
 
 // Example output
 // Before loop 3, 0, 2, 8, 0, 0, 0
@@ -90,7 +137,19 @@
       and lowest numbers.
  * 3. Log the highest and lowest number to the console
  */
-
+let largishArray = [2, 2, -3, 7, 4, 1, 7, 12, 8];
+let lowest = largishArray[0];
+let highest = largishArray[0];
+for (let largishArrayItems of largishArray){
+     if (lowest >= largishArrayItems){
+          lowest = largishArrayItems;
+     }
+     if (highest <= largishArrayItems){
+          highest = largishArrayItems;
+     }
+}
+console.log (`The lowest number is ${lowest}`);
+console.log (`The highest number is ${highest}`);
 
 // Example output
 // 2, 2, -3, 7, 4, 1, 7, 12, 8
